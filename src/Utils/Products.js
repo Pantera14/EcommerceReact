227d73f4858d92f14b7544a68ee5b6ceb5f439cc
Pantera <distribuidoracaba28@gmail.com>
2022-04-05 16:03:01 -0300

    desafio clase 5

---
+++ src/Utils/Products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id: 20,
+        name: 'Cerveza Andes Roja 473 ml (Six pack)',
+        stock: 2,
+        price: 1100,
+        image: ['https://super22deoctubre.com.ar/wp-content/uploads/2020/07/IMG_0469-scaled.jpg']
+    },
+    {
+        id: 21,
+        name: 'Cerveza Andes Rubia 473 ml (Six pack)',
+        stock: 4,
+        price: 1100,
+        image: ['https://super22deoctubre.com.ar/wp-content/uploads/2020/07/IMG_0473-scaled.jpg']
+}]
+
+module.exports = {
+    products,
+}
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,6 +1,21 @@
-const ItemListContainer = () =>{
+import ItemList from './ItemList';
+import customFetch from "../Utils/customFetch"
+import { useEffect, useState } from 'react';
+
+const {products} = require('../Utils/Products');
+
+const ItemListContainer = ({greeting}) =>{
+    const [datos, setDatos] = useState([]);
+
+    useEffect(() => {
+        customFetch(2000, products)
+            .then(result => setDatos(result))
+            .catch(err => console.log(err))
+    },[]);
+
+
     return(
-        <>Item List Container</>
+        <ItemList items={datos} />
     )
 }
 
